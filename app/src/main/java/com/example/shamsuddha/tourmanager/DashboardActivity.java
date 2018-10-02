a566a050db2768092c9306e4dce0781659bfd51b
shamsuddha <shamsuddha@gmail.com>
2018-10-02 13:58:17 +0600

    forget password added

--- app/src/main/java/com/example/shamsuddha/tourmanager/DashboardActivity.java
+++ app/src/main/java/com/example/shamsuddha/tourmanager/DashboardActivity.java
@@ -1,14 +1,40 @@
 package com.example.shamsuddha.tourmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() == null){
+            finish();
+            Intent i = new Intent(DashboardActivity.this, MainActivity.class);
+            startActivity(i);
+        }
+        user = firebaseAuth.getCurrentUser();
+
+
+
+
+
+
+
+
+
 
 
 
@@ -17,5 +43,42 @@ public class DashboardActivity extends AppCompatActivity {
 
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_after_login,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.action_home:
+                Intent home = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(home);
+                break;
 
+
+
+
+
+            case R.id.action_logout:
+                firebaseAuth.signOut();
+                finish();
+                Intent signOut = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(signOut);
+                break;
+
+
+
+            default:
+                Toast.makeText(this, "Something Went ", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //------------------ Menu Section End --------------------------------
 }
--- app/src/main/java/com/example/shamsuddha/tourmanager/PasswordResetActivity.java
+++ app/src/main/java/com/example/shamsuddha/tourmanager/PasswordResetActivity.java
@@ -1,19 +1,79 @@
 package com.example.shamsuddha.tourmanager;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PasswordResetActivity extends AppCompatActivity {
 
+
+    EditText resetPasswordEmailET;
+    Button resetPasswordButton;
+    private  ProgressBar progressBar;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_reset);
 
+        resetPasswordEmailET = findViewById(R.id.resetPasswordEmailET);
+        resetPasswordButton = findViewById(R.id.resetPasswordButton);
+        firebaseAuth = firebaseAuth.getInstance();
+
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                passwordReset();
+            }
+        });
 
 
 
 
 
     }
+
+    private void passwordReset() {
+        final ProgressDialog progress = new ProgressDialog(PasswordResetActivity.this);
+        progress.setMessage("Verifying...");
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.setIndeterminate(true);
+
+        String email = resetPasswordEmailET.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        progress.dismiss();
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(PasswordResetActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(PasswordResetActivity.this, "Please give a registered user email!", Toast.LENGTH_LONG).show();
+                        }
+
+                        // progressBar.setVisibility(View.GONE);
+                    }
+                });
+    }
+
+
 }
+
